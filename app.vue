73b95a54d6c8dba28beed4263a24d89abbebeee3
Sigve Hansen <sighan12@gmail.com>
2022-10-31 15:53:31 +0100

    fix(css): add css reset

--- app.vue
+++ app.vue
@@ -1,14 +1,6 @@
-<script setup lang="ts"></script>
-
 <template>
   <NuxtLayout>
     <NuxtLoadingIndicator />
     <NuxtPage />
   </NuxtLayout>
 </template>
-
-<style>
-body {
-  font-family: sans-serif;
-}
-</style>
--- components/Badge.vue
+++ components/Badge.vue
@@ -8,11 +8,20 @@
 
 <style lang="scss">
 .badge {
+  position: relative;
   padding: 0.2rem 0.5rem;
-  background-color: #eaeaea;
   color: #888;
   border-radius: 4px;
   text-transform: uppercase;
   letter-spacing: 0.5px;
+
+  &::before {
+    content: '';
+    background-color: currentColor;
+    opacity: 0.1;
+    position: absolute;
+    inset: 0;
+    border-radius: inherit;
+  }
 }
 </style>
