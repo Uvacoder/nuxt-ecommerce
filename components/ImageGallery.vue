4c3de1a05178ff83f0e67f19316dec2f1598f424
Sigve Hansen <sighan12@gmail.com>
2022-11-01 09:18:45 +0100

    fix: styling changes

--- components/ImageGallery.vue
+++ components/ImageGallery.vue
@@ -1,14 +1,55 @@
 <script setup lang="ts">
+import { nanoid } from 'nanoid'
+
+import Photoswipe from 'photoswipe'
+import PhotoswipeLightbox from 'photoswipe/lightbox'
+import 'photoswipe/style.css'
+
 import type { Product } from '@/types/product'
 
 defineProps<{
   images: Product['images']
 }>()
+
+const lightbox = ref(null)
+
+onMounted(() => {
+  if (!lightbox.value) {
+    lightbox.value = new PhotoswipeLightbox({
+      gallery: `#${nanoid()}`,
+      children: 'a',
+      pswpModule: Photoswipe,
+    })
+  }
+  lightbox.value.init()
+})
+
+onUnmounted(() => {
+  if (lightbox.value) {
+    lightbox.value.destroy()
+    lightbox.value = null
+  }
+})
 </script>
 
 <template>
   <div class="image-gallery">
-    <nuxt-img v-for="image in images" fit="cover" format="webp" :src="image" />
+    <a
+      v-for="(image, key) in images"
+      :key="key"
+      :href="image"
+      :data-pswp-width="200"
+      :data-pswp-height="200"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <nuxt-img
+        class="image-gallery__item"
+        fit="cover"
+        format="webp"
+        :src="image"
+      />
+    </a>
   </div>
 </template>
 
@@ -17,5 +58,9 @@ defineProps<{
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
   gap: 1rem;
+
+  &__item {
+    width: auto;
+  }
 }
 </style>
--- pages/products/[productId].vue
+++ pages/products/[productId].vue
@@ -10,15 +10,32 @@ const { data: product } = await useFetch<Product>(
 </script>
 
 <template>
-  <Container>
-    <h1>{{ product.title }}</h1>
+  <Container class="product-info">
     <nuxt-img
       fit="cover"
       format="webp"
       :src="product.thumbnail"
       :alt="product.title"
     />
-    <p>{{ product.description }}</p>
-    <ImageGallery :images="product.images" />
+    <div class="product-info__details">
+      <h1>{{ product.title }}</h1>
+      <p>{{ product.description }}</p>
+    </div>
+    <ImageGallery class="product-info__gallery" :images="product.images" />
   </Container>
 </template>
+
+<style lang="scss">
+.product-info {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(1fr, 2fr));
+
+  &__gallery {
+    margin-top: 4rem;
+
+    @media (min-width: 600px) {
+      grid-column: span 2;
+    }
+  }
+}
+</style>
